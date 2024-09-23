77c475d0d88c09310e7d82166f1138b35868e216
Django-Fakkeldij <djangofakkeldij@gmail.com>
2024-09-23 10:51:43 +0200

    Node Connction class

--- server/protocol.py
+++ server/protocol.py
@@ -1,50 +1,57 @@
 import time
-from enum import Enum
 
 import serial
 
 
-def debug(*args):
-    print("DEBUG |", *args)
+class NodeConnection:
+    _conn: serial.Serial
+    _debug: bool
+    _name: str
 
+    def __init__(self, port: str, debug=False, name: str = "Node") -> None:
+        self._debug = debug
+        self._name = name
+        self._conn = serial.Serial(port=port, baudrate=115200, timeout=0.5)
+        # WARNING: MUST FIRST START WITH READING MESSAGE
+        self.debug(self.readMessage())
 
-conn = serial.Serial(port="COM4", baudrate=115200, timeout=2)
+    def debug(self, *args):
+        if not self._debug:
+            return
+        t = time.localtime(time.time())
+        ft = time.strftime("%Y-%m-%d %H:%M:%S", t)
+        print(f"{ft} | {self._name} | DEBUG |", *args)
 
+    def readMessage(self) -> bytearray:
+        a = bytearray()
 
-def readMessage() -> bytearray:
-    a = bytearray()
-
-    # Throw away everything if it does not match start char
-    length = 0
-    while True:
-        rb = conn.read()
-        if rb == bytes():
-            debug("...")
+        # Throw away everything if it does not match start char
+        length = 0
+        while True:
+            rb = self._conn.read()
+            if rb == bytes():
+                self.debug("...")
+                continue
+            # mark "<" as startbyte
+            if rb == b"<":
+                self.debug("got start byte")
+                length = int.from_bytes(self._conn.read(), "little")
+                self.debug("got length byte: ", length)
+                break
             continue
-        # mark "<" as startbyte
-        if rb == b"<":
-            debug("got start byte")
-            length = int.from_bytes(conn.read(), "little")
-            debug("got length byte: ", length)
-            break
-        continue
 
-    for _ in range(length):
-        rb = conn.read()
-        a += rb
+        for _ in range(length):
+            rb = self._conn.read()
+            a += rb
 
-    return a
+        return a
 
-
-def writeMessage(m: bytes):
-    conn.write("<".encode("ascii"))
-    lb = len(m).to_bytes(1, "little")
-    conn.write(lb)
-    debug("lb: ", int.from_bytes(lb, "little"))
-    conn.write(m)
+    def writeMessage(self, m: bytes):
+        self._conn.write("<".encode("ascii"))
+        lb = len(m).to_bytes(1, "little")
+        self._conn.write(lb)
+        self.debug("lb: ", int.from_bytes(lb, "little"))
+        self._conn.write(m)
 
 
-debug("Starting")
-debug(readMessage().decode("ascii"))
-writeMessage("ping".encode("ascii"))
-debug(readMessage().decode("ascii"))
+# c = NodeConnection("COM4", True)
