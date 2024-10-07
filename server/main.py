52112d7eb4132ddcf16fd1a3634ad1ec2f221707
Django-Fakkeldij <djangofakkeldij@gmail.com>
2024-10-07 12:36:51 +0200

    Queue mechanism (untested)

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import protocol
 from motor import MotorContext
 
-node1 = protocol.NodeConnection("COM5", False, "Node 1")
+node1 = protocol.NodeConnection("COM4", True, "Node 1")
 
 movement_array = np.array([[150, 50], [140, 36]])
 
@@ -21,11 +21,13 @@ controller = control.Control(motor1, motor2, node1)
 motor1Inv = True
 motor2Inv = False
 
+for i in range(0, 3200 * 10, 800):
+    controller.sendSteps(i, i)
 
-controller.setOrigin(motor1Inv=motor1Inv, motor2Inv=motor2Inv)
-controller.setOrigin(
-    motor1Inv=motor1Inv, motor2Inv=motor2Inv, offset=np.array([15, 45])
-)
-controller.moveTo(np.array([50, 20]), motor1Inv=motor1Inv, motor2Inv=motor2Inv)
-time.sleep(2)
-controller.moveTo(np.array([50, 180]), motor1Inv=motor1Inv, motor2Inv=motor2Inv)
+# controller.setOrigin(motor1Inv=motor1Inv, motor2Inv=motor2Inv)
+# controller.setOrigin(
+#     motor1Inv=motor1Inv, motor2Inv=motor2Inv, offset=np.array([15, 45])
+# )
+# controller.moveTo(np.array([50, 20]), motor1Inv=motor1Inv, motor2Inv=motor2Inv)
+# time.sleep(2)
+# controller.moveTo(np.array([50, 180]), motor1Inv=motor1Inv, motor2Inv=motor2Inv)
